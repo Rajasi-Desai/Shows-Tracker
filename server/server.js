8271d4634cc09f0ab403aaf9ffc0f9c4c34aa658
Rajasi Desai <rajasi.desai18@gmail.com>
2026-09-10 20:15:50 +0530

    connect to TMDB in server.js

--- server/server.js
+++ server/server.js
@@ -13,3 +13,34 @@ app.get('/api/data', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+const apiKey = process.env.TMDB_API_KEY;
+app.get('/api/account', async (req, res) => {
+    try {
+        const url = 'https://api.themoviedb.org/3/account/23700911';
+        const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json', 
+            Authorization: `Bearer ${apiKey}`}
+        };
+
+
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            throw new Error(`TMDB API error: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        res.json(data);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch data from TMDB' });
+    }
+
+});
+
+
